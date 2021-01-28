e8d81e092d1dc49b87d84400b4cecdfbc0282387
Anton Siarko <anton_siarko@epam.com>
2021-01-28 16:42:50 +0300

    fix: removed .idea and .vs folders

--- WpfApplication1/WpfApplication1/Config.cs
+++ WpfApplication1/WpfApplication1/Config.cs
@@ -2,9 +2,6 @@
 {
     public class Config
     {
-        // public const string DB_PATH = @"E:\mydb";
-        // public const string LOG_FILE_PATH = @"E:\logs.txt";
-        
         public const string DB_PATH = @"C:\Users\anton\source\repos\disinfectant-calculator-wpf\mydb";
         public const string LOG_FILE_PATH = @"C:\Users\anton\source\repos\disinfectant-calculator-wpf\logs.txt";
     }
