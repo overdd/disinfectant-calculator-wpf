b76235433a5f2015522574cd26c40b90c7ddde02
Anton Siarko <anton_siarko@epam.com>
2021-01-29 10:51:05 +0300

    fix: database naming fixed

--- WpfApplication1/WpfApplication1/Config.cs
+++ WpfApplication1/WpfApplication1/Config.cs
@@ -2,7 +2,7 @@
 {
     public class Config
     {
-        public const string DB_PATH = @"C:\Users\anton\source\repos\disinfectant-calculator-wpf\mydb";
+        public const string DB_PATH = @"C:\Users\anton\source\repos\disinfectant-calculator-wpf\mydb.sqlite";
         public const string LOG_FILE_PATH = @"C:\Users\anton\source\repos\disinfectant-calculator-wpf\logs.txt";
     }
 }
